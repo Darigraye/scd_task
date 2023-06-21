cfc2929f7d6f1750434ebb36d6fe790d67e853f0
Evgeniy Shpachkov <evgeniy.shpachkov@glowbyteconsulting.com>
2023-06-21 17:23:03 +0300

    progress in making function scd2

--- task 2 (scd2).sql
+++ task 2 (scd2).sql
@@ -83,9 +83,10 @@ insert into custom_TMP_clients(al_id, client_id, name, surname, t_changed, t_loa
 values (al_ids.nextval, client_ids.nextval, 'Edward', 'Paker', '1', load_ids.nextval, sysdate, 1);
 
 insert into custom_TMP_clients(al_id, client_id, name, surname, t_changed, t_load_id, begin_date, t_is_active) 
-values (al_ids.nextval, client_ids.nextval, 'Tom', 'Shell', '3', load_ids.nextval, sysdate, 1);
+values (al_ids.nextval, client_ids.nextval, 'Tom', 'Shell', '1', load_ids.nextval, sysdate, 1);
 
 select * from custom_TMP_clients;
+delete from custom_TMP_clients;
 
 ------------------------------------------
 
@@ -115,13 +116,86 @@ select * from i_TMP_clients;
 create or replace procedure scd2
        (load_id in number)
 is
-       cursor cur_dublicates is
-              
-begin
+       count_dublicates number;
+       dublicated exception;
        
+       cursor i_client_cur is
+                 select * from i_TMP_clients order by id;
+       i_client_rec i_client_cur%rowtype;
+          
+       cursor client_cur is
+                 select * from custom_TMP_clients where t_is_active = 1 order by client_id;
+       client_rec client_cur%rowtype;
+begin
+       with cd as (
+       select client_id, count(*) over(partition by client_id) count_dubs
+       from custom_TMP_clients
+       where t_is_active = 1
+       )
+       
+       select max(count_dubs) into count_dublicates from cd;
+       
+       if count_dublicates > 1
+       then
+         raise dublicated;
+       end if;
+       
+       open i_client_cur;
+       open client_cur;
+       
+       fetch i_client_cur into i_client_rec;     
+       fetch client_cur into client_rec;
+       
+       loop
+                   if i_client_rec.id = client_rec.client_id 
+                   then
+                       if i_client_rec.name != client_rec.name or i_client_rec.surname != client_rec.surname -- update
+                       then
+                          update custom_TMP_clients set name = i_client_rec.name, 
+                                                        surname = i_client_rec.surname,
+                                                        t_changed = '2',
+                                                        t_load_id = load_id,
+                                                        al_udate = sysdate,
+                                                        t_is_active = 0,
+                                                        end_date = to_timestamp(to_char(trunc(current_timestamp - 1, 'DDD')) || ' 23:59:00', 'DD-Mon-RR HH24:MI:SS')                                                     
+                          where client_id = i_client_rec.id and t_is_active = 1;    
+                       end if;
+                       
+                         fetch i_client_cur into i_client_rec;
+                         exit when i_client_cur%notfound;
+               
+                         fetch client_cur into client_rec;
+                         exit when client_cur%notfound;
+                   else
+                     insert into custom_TMP_clients(id, 
+                                               name, 
+                                               surname,
+                                               al_cdate,
+                                               t_changed,
+                                               al_udate,
+                                               t_load_id
+                                               )
+                     values (i_client_rec.id,
+                             i_client_rec.name,
+                             i_client_rec.surname,
+                             i_client_rec.al_cdate,
+                             '1',
+                             sysdate,
+                             load_id);
+                     
+                     fetch i_client_cur into i_client_rec;
+                     exit when i_client_cur%notfound;
+                   end if;                    
+       end loop;
+       
+exception
+  when dublicated then
+    dbms_output.put_line('there are dublicated active records in target table');
 end;
 
 begin
        scd2(170);
 end;
+
+select to_timestamp(to_char(trunc(current_timestamp - 1, 'DDD')) || ' 23:59:00', 'DD-Mon-RR HH24:MI:SS') from dual;
        
